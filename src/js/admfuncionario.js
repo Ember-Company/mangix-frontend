8720b75d7d43a0d341406109fbe81d3d68edc985
Nathan Grima <nathangrima.m1@gmail.com>
2024-04-29 12:50:25 -0300

    tela de adm/funcionarios

---
+++ src/js/admfuncionario.js
@@ -0,0 +1,65 @@
+var selectElement = document.getElementById("yearSelect");
+
+var currentYear = new Date().getFullYear();
+
+
+for (var year = currentYear; year >= 1900; year--) {
+    var option = document.createElement("option");
+    option.text = year;
+    option.value = year;
+    selectElement.add(option);
+}
+
+
+function criarDivs() {
+    const dynamicContent = document.getElementById('dynamicContent');
+    const numDivs = new Date().getDate();
+
+    for (let i = 0; i < numDivs; i++) {
+        const div = document.createElement('div');
+        div.classList.add('dias');
+
+        const innerHTML = `
+            <div>
+                <p>${i + 1}</p>
+            </div>
+            <div>
+                <p>${i === 0 ? "Nathan" : ""}</p>
+            </div>
+            <div>
+                <p>${i === 0 ? "00000" : ""}</p>
+            </div>
+            <div>
+            <p>${i === 0 ? "01/01/24" : ""}</p>
+            </div>
+            <div>
+            <p>${i === 0 ? "08:00" : ""}</p>
+            </div>
+            <div>
+            <p>${i === 0 ? "12:00" : ""}</p>
+            </div>
+            <div>
+            <p>${i === 0 ? "13:00" : ""}</p>
+            </div>
+            <div>
+            <p>${i === 0 ? "18:00" : ""}</p>
+            </div>
+            <div>
+                <box-icon name='file-pdf' type='solid'></box-icon>
+            </div>
+        `;
+
+        div.innerHTML = innerHTML;
+        
+        if (div.querySelector('div:nth-child(2) p').textContent.trim().length > 0) {
+            div.querySelector('div:last-child').style.display = 'block'; // Exibe o ícone
+        } else {
+            div.querySelector('div:last-child').style.display = 'none'; // Oculta o ícone
+        }
+        
+        dynamicContent.appendChild(div);
+    }
+}
+
+// Chamada da função ao carregar a página
+window.onload = criarDivs;
