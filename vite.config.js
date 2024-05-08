30eb1549825fe8b566147880b6191f3055c99d17
dannyk-dev <kruger.dkk@gmail.com>
2024-05-08 19:29:38 -0300

    added try-catch block

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,19 @@ async function extractPageRoutes(appDir) {
   let input = {};
 
   const traverse = async (dir) => {
-    const folders = await fs.promises.readdir(dir, { withFileTypes: true });
+    try {
+      const folders = await fs.promises.readdir(dir, { withFileTypes: true });
 
-    for (const folder of folders) {
-      const route = path.join(dir, folder.name);
+      for (const folder of folders) {
+        const route = path.join(dir, folder.name);
 
-      folder.isDirectory() && (await traverse(route));
+        folder.isDirectory() && (await traverse(route));
 
-      const key = folder.name.split(".")[0];
-      input[key] = path.relative(appDir, route);
+        const key = folder.name.split(".")[0];
+        input[key] = path.relative(appDir, route);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
